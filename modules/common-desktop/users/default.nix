2e3f260304246ed6496817e60dbebfe44d40338e
Rutger Putter <rutger@prutser.net>
2025-05-21 12:58:28 +0200

    feat: create common desktop users

---
+++ modules/common-desktop/users/default.nix
@@ -0,0 +1,44 @@
+{ pkgs, ... }:
+{
+  imports = [ ];
+
+  users = {
+    mutableUsers = true;
+    users = {
+      rputter = {
+        description = "Rutger Putter";
+        home = "/home/rputter";
+        group = "users";
+        createHome = true;
+        homeMode = "700";
+        isSystemUser = false;
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        extraGroups = [ "networkmanager" "wheel" "gamemode" "libvirtd"];
+        packages = with pkgs; [
+          signal-desktop
+          unstable.bitwarden-desktop
+          vscode
+          vlc
+          haruna
+          thunderbird-latest
+          heroic
+          protonup-qt
+          finamp
+          jellyfin-media-player
+          gimp-with-plugins
+          mission-center
+          bottles
+          isoimagewriter
+          kdenlive
+          nextcloud-client
+          spotube
+          boatswain
+          teams-for-linux
+          moonlight-qt
+          prusa-slicer
+        ];
+      };
+    };
+  };
+}
