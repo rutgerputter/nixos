38f3fe9ef4a84917a3bc52945ebdd1dda9009837
Rutger Putter <rutger@prutser.net>
2025-05-21 14:51:32 +0200

    feat: flesh out nextcloud config

--- applications/nextcloud-aio/default.nix
+++ applications/nextcloud-aio/default.nix
@@ -13,4 +13,42 @@
     postgresql
     redis
   ];
+
+  services = {
+    nextcloud = {
+      appstoreEnable = true;
+      autoUpdateApps.enable = false;
+      caching.apcu = true;
+      caching.memcached = true;
+      caching.redis = true;
+      cli.memoryLimit = "1G";
+      config = {
+        adminuser = "rputter";
+        dbtype = "pgsql";
+      };
+      configureRedis = true;
+      database.createLocally = true;
+      datadir = "/mnt/nextcloud";
+      enable = true;
+      enableImagemagick = true;
+      home = "/var/lib/nextcloud";
+      hostName = "ncdemo.prutser.net";
+      https = true;
+      maxUploadSize = "20G";
+      nginx.recommendedHttpHeaders = true;
+      notify_push.enable = true;
+      settings = {
+        overwriteprotocol = "https";
+        trusted_domains = "ncdemo.prutser.net";
+        trusted_proxies = [ "caddy.services.prutser.net" ];
+      };
+    };
+    nextcloud-whiteboard-server = {
+      enable = true;
+      secrets = [ ];
+      settings = {
+        NEXTCLOUD_URL = "https://nextcloud.example.org";
+      };
+    };
+  };
 }
