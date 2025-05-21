c28944d7b262f75b1a1858b83ce12ffd963ec483
Rutger Putter <rutger@prutser.net>
2025-05-21 10:32:46 +0200

    feat: move to common module structure for environment

--- hosts/nb-rputter/modules/environment/default.nix
+++ hosts/nb-rputter/modules/environment/default.nix
@@ -1,72 +1,10 @@
 { pkgs, ... }:
 {
+  imports = [
+    ../../../../modules/common-desktop/environment
+  ];
   environment.systemPackages = with pkgs; [
-    exfat
-    exfatprogs
-    gcc
-    usbutils
-    pciutils
-    dmidecode
-    libva-utils
-    intel-gpu-tools
-    git
-    direnv
-    nix-search-cli
-    nixos-generators
-    nil
-    nixd
-    hunspell
-    hunspellDicts.en_US
-    hunspellDicts.nl_NL
-    python3Full
-    htop
-    yakuake
-    throttled
-    epson-escpr
-    epson-escpr2
-    epsonscan2
-    (hiPrio papirus-icon-theme)
-    lshw
-    kdePackages.partitionmanager
-    kdePackages.kate
-    kdePackages.filelight
-    kdePackages.kcalc
-    kdePackages.kscreenlocker
-    kdePackages.markdownpart
-    kdePackages.sonnet
-    android-tools
-    gearlever
-    tailscale
-    ktailctl
-    alsa-plugins
-    libreoffice
-    solaar
-    lsd
-    fzf
-    fastfetch
-    kitty
-    tdrop
     libfprint-2-tod1-vfs0090
-    python312Packages.pip
-    mono
-    sdrpp
-    haskellPackages.rtlsdr
-    rtl-sdr-librtlsdr
-    soapyrtlsdr
-    retroarch
   ];
 
-  # FONTS
-  fonts.packages = with pkgs; [
-    inter
-    noto-fonts
-    fira-code
-    noto-fonts-cjk-sans
-    jetbrains-mono
-    font-awesome
-    terminus_font
-    victor-mono
-    (nerdfonts.override {fonts = ["JetBrainsMono"];}) # stable banch
-    (nerdfonts.override {fonts = ["FantasqueSansMono"];}) # stable banch
-  ];
 }
---
+++ modules/common-desktop/environment/fonts.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  # FONTS
+  fonts.packages = with pkgs; [
+    inter
+    noto-fonts
+    fira-code
+    noto-fonts-cjk-sans
+    jetbrains-mono
+    font-awesome
+    terminus_font
+    victor-mono
+    (nerdfonts.override {fonts = ["JetBrainsMono"];}) # stable banch
+    (nerdfonts.override {fonts = ["FantasqueSansMono"];}) # stable banch
+  ];
+}
