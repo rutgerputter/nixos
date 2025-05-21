02e54d691f04fa61506a25370439008e7f73f091
Rutger Putter <rutger@prutser.net>
2025-05-21 15:06:01 +0200

    chore: add SSH key for rputter user

--- modules/common-workload/users/default.nix
+++ modules/common-workload/users/default.nix
@@ -13,6 +13,7 @@
         isNormalUser = true;
         shell = pkgs.zsh;
         extraGroups = [ "wheel"];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXgN/t1XjRbHwcsfihSmx+GiRoPBVU0AzL1o8xTZXNJ" ];
 #         packages = with pkgs; [
 #         ];
       };
