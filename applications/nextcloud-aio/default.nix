f12e3612340081465a2d76de48c0c2669a499775
Rutger Putter <rutger@prutser.net>
2025-05-26 21:53:03 +0200

    feat: configure netcloud

--- applications/nextcloud-aio/default.nix
+++ applications/nextcloud-aio/default.nix
@@ -1,55 +1,48 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    nextcloud-notify_push
-    nextcloud-whiteboard-server
-    # talk?
-    # talk-recording?
-    collabora-online
-    # elasticsearch
-    imaginary
-    clamav
-    postgresql
-    redis
-  ];
-
-  services = {
-    nextcloud = {
-      appstoreEnable = true;
-      autoUpdateApps.enable = false;
-      caching.apcu = true;
-      caching.memcached = true;
-      caching.redis = true;
-      cli.memoryLimit = "1G";
-      config = {
-        adminuser = "rputter";
-        adminpassFile = "/opt/adminpassFile.txt";
-        dbtype = "sqlite";
-      };
-      configureRedis = true;
-      database.createLocally = true;
-      datadir = "/mnt/nextcloud";
-      enable = true;
-      enableImagemagick = true;
-      home = "/var/lib/nextcloud";
-      hostName = "ncdemo.prutser.net";
-      https = true;
-      maxUploadSize = "20G";
-      nginx.recommendedHttpHeaders = true;
-      package = pkgs.nextcloud31;
-      notify_push.enable = true;
-      settings = {
-        overwriteprotocol = "https";
-        trusted_domains = [ "ncdemo.prutser.net" ];
-        trusted_proxies = [ "caddy.services.prutser.net" ];
-      };
+  environment.etc."nextcloud-admin-pass".text = "ChangeMe!";
+  services.nextcloud = {
+    enable = true;
+    enableImagemagick = true;
+    appstoreEnable = true;
+    configureRedis = true;
+    datadir = "/mnt/nextcloud";
+    package = pkgs.nextcloud31;
+    hostName = "ncdemo.prutser.net";
+    database.createLocally = true;
+    notify_push.enable = true;
+    notify_push.nextcloudUrl = "http://vm-nextcloud-demo.services.prutser.net";
+    config = {
+      adminpassFile = "/etc/nextcloud-admin-pass";
+      dbtype = "pgsql";
     };
-    nextcloud-whiteboard-server = {
-      enable = true;
-      secrets = [ ];
-      settings = {
-        NEXTCLOUD_URL = "https://ncdemo.prutser.net";
-      };
+    maxUploadSize = "1G";
+    phpOptions = {
+      "opcache.interned_strings_buffer" = "32";
+      "opcache.revalidate_freq" = "60";
+    };
+    settings = {
+      trusted_domains = [ "ncdemo.prutser.net" "vm-nextcloud-demo.services.prutser.net" ];
+      trusted_proxies = [ "10.0.10.102" "10.0.10.113" ];
+      log_type = "file";
+      default_phone_region = "NL";
+      overwriteprotocol = "https";
+      enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
     };
   };
+  services.nextcloud-whiteboard-server.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
 }
