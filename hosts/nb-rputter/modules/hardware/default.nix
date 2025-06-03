6af138f8ebaa2ab2106c71764b5a3ee9fa625d18
installer <installer@gmail.com>
2025-06-03 13:48:24 +0200

    chore: calib-data.bin is 0B, can't use it this way

--- hosts/nb-rputter/modules/hardware/default.nix
+++ hosts/nb-rputter/modules/hardware/default.nix
@@ -8,8 +8,7 @@
 
   services."06cb-009a-fingerprint-sensor" = {
     enable = true;
-    backend = "libfprint-tod";
-    calib-data-file = ./calib-data.bin;
+    backend = "python-validity";
   };
 
   hardware = {
