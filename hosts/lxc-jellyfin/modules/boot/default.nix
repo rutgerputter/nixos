0ea92644f16e1ab1b792a509b53cc6a045592d41
Rutger Putter <rutger@prutser.net>
2025-06-10 10:38:09 +0200

    feat: adopt lxc config for jellyfin

--- hosts/lxc-jellyfin/modules/boot/default.nix
+++ hosts/lxc-jellyfin/modules/boot/default.nix
@@ -5,7 +5,22 @@
   ];
 
   proxmoxLXC = {
-    manageNetwork = true;
     privileged = true;
+    manageNetwork = false;
+    manageHostName = false;
   };
+
+  boot.isContainer = true;
+
+  console.enable = true;
+
+  systemd.services."getty@" = {
+    unitConfig.ConditionPathExists = ["" "/dev/%I"];
+  };
+
+  systemd.suppressedSystemUnits = [
+    "dev-mqueue.mount"
+    "sys-kernel-debug.mount"
+    "sys-fs-fuse-connections.mount"
+  ];
 }
--- hosts/lxc-jellyfin/modules/services/default.nix
+++ hosts/lxc-jellyfin/modules/services/default.nix
@@ -1,10 +1,7 @@
-{ lib, ... }:
+{ ... }:
 
 {
   imports = [
     ../../../../modules/common-workload/services
   ];
-  services.cloud-init.enable = true;
-  # Enable QEMU Guest for Proxmox
-  services.qemuGuest.enable = lib.mkDefault true;  
 }
