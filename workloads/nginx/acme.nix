b37e6b122b65214fb7629cdcbf5022bda71ce391
Rutger Putter <rutger@prutser.net>
2025-06-10 17:31:29 +0200

    feat: move to production for ACME

--- workloads/nginx/acme.nix
+++ workloads/nginx/acme.nix
@@ -7,7 +7,7 @@
     acceptTerms = true;
     maxConcurrentRenewals = 10;
     defaults.email = "rutger@prutser.net";
-    defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+    # defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
     defaults.dnsProvider = "cloudflare"; # Cloudflare DNS API
     defaults.dnsPropagationCheck = true;
     defaults.dnsResolver = "1.1.1.1:53";
