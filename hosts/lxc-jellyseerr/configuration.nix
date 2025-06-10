6f6cba68869de061e4fb243b09e2f0f9bf76b2be
Rutger Putter <rutger@prutser.net>
2025-06-10 13:56:14 +0200

    feat: move config and add jellyseerr

--- hosts/lxc-jellyseerr/configuration.nix
+++ hosts/lxc-jellyseerr/configuration.nix
@@ -4,5 +4,17 @@
 {
   imports = [
     ../../workloads/jellyseerr
+    ./modules/boot
+    ./modules/environment
+    ./modules/home-manager
+    ./modules/localization
+    ./modules/networking
+    ./modules/nix
+    ./modules/nixpkgs
+    ./modules/programs
+    ./modules/security
+    ./modules/services
+    ./modules/system
+    ./modules/users    
   ];
 }
