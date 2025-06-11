9e70eec9c7a28acf71b8ae223ef8f2653b95d0c5
Rutger Putter <rutger@prutser.net>
2025-06-11 14:58:41 +0200

    fixup: audiobookshelf issues

--- workloads/audiobookshelf/mounts.nix
+++ workloads/audiobookshelf/mounts.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   fileSystems."/var/lib/audiobookshelf" = {
-    device = "10.0.99.10:/mnt/ssd_pool/dockervols/jellyfin/music-assistant";
+    device = "10.0.99.10:/mnt/ssd_pool/dockervols/audiobookshelf";
     fsType = "nfs";
     options = [ "nfsvers=4.2" "nolock" "soft" "rw" "nconnect=6" ];
   };
