f539520da457da628d0d0d3eed8fa3b0c46ff44a
Rutger Putter <rutger@prutser.net>
2025-06-12 14:24:36 +0200

    feat: switch tb-rputter to gnome to accomodate tablet formfactor better

--- hosts/tb-rputter/modules/services/default.nix
+++ hosts/tb-rputter/modules/services/default.nix
@@ -5,14 +5,14 @@
   services.displayManager.sddm.enable = false;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
-  services.displayManager.defaultSession = "plasma";
+  services.displayManager.defaultSession = "gnome";
   services.displayManager.autoLogin = {
     enable = false;
   };
   # Disable the X11 windowing system.
   services.xserver.enable = false;
 
-  services.desktopManager.plasma6.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   systemd.services.NetworkManager-wait-online.enable = false;
 
