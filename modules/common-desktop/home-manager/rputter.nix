34ec4066e407f5d2fe567f7f1cfdd8d5c1b2a55c
Rutger Putter <rutger@prutser.net>
2025-06-16 13:15:33 +0200

    feat: fix SSH AUTH SOCK for GNOME

--- modules/common-desktop/home-manager/rputter.nix
+++ modules/common-desktop/home-manager/rputter.nix
@@ -62,6 +62,21 @@
     };
   };
 
+  xdg.configFile."autostart/gnome-keyring-ssh.desktop" = {
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=SSH Key Agent
+      Comment=GNOME Keyring: SSH Agent
+      Exec=/run/wrappers/bin/gnome-keyring-daemon --start --components=ssh
+      OnlyShowIn=GNOME;Unity;MATE;
+      X-GNOME-Autostart-Phase=PreDisplayServer
+      X-GNOME-AutoRestart=false
+      X-GNOME-Autostart-Notify=true
+      Hidden=true
+    '';
+  };
+
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
