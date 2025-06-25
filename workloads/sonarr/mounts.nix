0e065e09e29980658bb86d60750b6d96c7e2253b
Rutger Putter <rutger@prutser.net>
2025-06-25 13:08:01 +0200

    feat: add video mounts to radarr and sonarr

--- workloads/sonarr/mounts.nix
+++ workloads/sonarr/mounts.nix
@@ -5,4 +5,9 @@
     fsType = "nfs";
     options = [ "nfsvers=4.2" "nolock" "soft" "rw" "nconnect=6" ];
   };
+  fileSystems."/data/video" = {
+    device = "10.0.99.10:/mnt/hdd_pool/video";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "nolock" "soft" "rw" "nconnect=6" ];
+  };
 }
