04ae8edcdc9de2e9bf2cb2f0d3e543b8875b592d
Rutger Putter <rutger@prutser.net>
2025-06-29 21:16:53 +0200

    feat: add correct filesystems for nb2-rputter

--- hosts/nb2-rputter/modules/boot/default.nix
+++ hosts/nb2-rputter/modules/boot/default.nix
@@ -18,40 +18,32 @@
   boot.loader.timeout = 0;
 
   # Ensure boot works with all appropriate storage devices and protocols.
-  boot.initrd.luks.devices."luks-ca4832e4-ab65-4015-a638-6291f324e999".device = "/dev/disk/by-uuid/ca4832e4-ab65-4015-a638-6291f324e999";
-  boot.initrd.luks.devices."luks-dfbc71e5-c3d0-48c6-8537-29f33435d4f1".device = "/dev/disk/by-uuid/dfbc71e5-c3d0-48c6-8537-29f33435d4f1";
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "usbhid" "uas" "sd_mod" ];
-  boot.initrd.kernelModules = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "uas" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.systemd.enable = true;
   boot.initrd.supportedFilesystems = [ ];
   boot.initrd.verbose = false;
 
+
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6716ee49-6d4c-46fc-b141-f2a8c222c6ff";
+    { device = "/dev/disk/by-uuid/40f2f4c0-13ac-4db1-9d82-5f7af9c1c0a0";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1985-1E16";
+    { device = "/dev/disk/by-uuid/B4B2-B145";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/e1a8e8fc-69c3-4dda-9fd5-9e1084c92a58"; }
+    [ { device = "/dev/disk/by-uuid/e7817162-c423-4505-a0ca-efe093197134"; }
     ];
-
-  fileSystems."/home/rputter/GamesSSD" =
-    { device = "/dev/disk/by-uuid/b3f7f216-e285-4e8d-8139-cf40004cb344";
-      fsType = "ext4";
-    };
 
   # Plymouth config
   boot.plymouth.enable = true;
   boot.plymouth.theme = "bgrt";
   boot.plymouth.logo = "${pkgs.nixos-icons}/share/icons/hicolor/48x48/apps/nix-snowflake-white.png";
 
-  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   # Enable "Silent Boot"
