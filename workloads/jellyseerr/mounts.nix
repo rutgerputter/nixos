5de5cf16ee809e2608cc24d50abd78d6cd8a8529
Rutger Putter <rutger@prutser.net>
2025-07-07 22:24:25 +0200

    fix: jellyseer mount point

--- workloads/jellyseerr/mounts.nix
+++ workloads/jellyseerr/mounts.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  fileSystems."/var/lib/private/jellyseerr/config" = {
+  fileSystems."/var/lib/private/jellyseerr" = {
     device = "10.0.99.10:/mnt/ssd_pool/dockervols/jellyfin/jellyseerr";
     fsType = "nfs";
     options = [ "nfsvers=4.2" "nolock" "soft" "rw" "nconnect=6" ];
