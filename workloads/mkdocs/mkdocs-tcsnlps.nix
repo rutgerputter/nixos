7d53a21e08b5a71491f454c155081f4e5da1982f
Rutger Putter <rutger@prutser.net>
2025-07-09 13:44:14 +0200

    fix: git pull mechanism

--- workloads/mkdocs/mkdocs-tcsnlps.nix
+++ workloads/mkdocs/mkdocs-tcsnlps.nix
@@ -11,44 +11,19 @@
   };
 
   systemd.services."pull-mkdocs" = {
-    enable = false;
     description = "MKDocs remote pull";
     after = [ "network-online.target" ];
     wants = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.git}/bin/git pull";
-      DynamicUser = true;
-      AmbientCapabilities = "";
-      CapabilityBoundingSet = [ "" ];
-      DevicePolicy = "closed";
-      LockPersonality = true;
-      MemoryDenyWriteExecute = true;
-      ProcSubset = "pid";
-      ProtectClock = true;
-      ProtectControlGroups = true;
-      ProtectHome = true;
-      ProtectHostname = true;
-      ProtectKernelLogs = true;
-      ProtectKernelModules = true;
-      ProtectKernelTunables = true;
-      ProtectProc = "invisible";
-      RestrictAddressFamilies = [
-        "AF_INET"
-        "AF_INET6"
-        "AF_NETLINK"
-      ];
-      RestrictNamespaces = true;
-      RestrictRealtime = true;
-      SystemCallArchitectures = "native";
-      SystemCallFilter = [
-        "@system-service"
-        "~@privileged @resources"
-      ];
-      RestrictSUIDSGID = true;
-      UMask = "0077";
       WorkingDirectory = "/var/lib/mkdocs";
+      ExecStart = "${pkgs.git}/bin/git pull";
+      Type = "oneshot";
+      User = "root";
     };
   };
 
+  programs.git.config = {
+    safe.directory = "/var/lib/private/mkdocs";
+  };
 }
