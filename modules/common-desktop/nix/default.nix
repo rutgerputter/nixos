8408b8418ee0fee40d964a52a56774d01e210c5e
Rutger Putter <rutger@prutser.net>
2025-07-16 15:46:56 +0200

    chore: add trusted public-keys

--- modules/common-desktop/nix/default.nix
+++ modules/common-desktop/nix/default.nix
@@ -3,8 +3,9 @@
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
-      substituters = [ "https://cache.nixos.org" "https://colmena.cachix.org"];
-      trusted-substituters = [ "https://cache.nixos.org" "https://colmena.cachix.org"];
+      substituters = [ "https://colmena.cachix.org"];
+      trusted-substituters = [ "https://colmena.cachix.org"];
+      trusted-public-keys = [ "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg=" ];
     };
     optimise = {
       automatic = true;
--- modules/common-workload/nix/default.nix
+++ modules/common-workload/nix/default.nix
@@ -3,8 +3,9 @@
     settings = {
       trusted-users = [ "root" "@wheel" ];
       experimental-features = [ "nix-command" "flakes" ];
-      substituters = [ "https://cache.nixos.org" "https://colmena.cachix.org"];
-      trusted-substituters = [ "https://cache.nixos.org" "https://colmena.cachix.org"];
+      substituters = [ "https://colmena.cachix.org" ];
+      trusted-substituters = [ "https://colmena.cachix.org" ];
+      trusted-public-keys = [ "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg=" ];
     };
     gc = {
       automatic = true;
