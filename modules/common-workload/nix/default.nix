f6639c122055b1b83916ea2102e86d2e6f97f9fb
Rutger Putter <rutger@prutser.net>
2025-07-17 10:52:36 +0200

    fix: pipeline

--- modules/common-workload/nix/default.nix
+++ modules/common-workload/nix/default.nix
@@ -4,7 +4,7 @@
       trusted-users = [ "root" "@wheel" ];
       experimental-features = [ "nix-command" "flakes" ];
       substituters = [ "https://colmena.cachix.org" ];
-      trusted-substituters = [ "https://colmena.cachix.org" ];
+      trusted-substituters = [ "https://cache.nixos.org" "https://colmena.cachix.org" ];
       trusted-public-keys = [ "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg=" ];
     };
     gc = {
