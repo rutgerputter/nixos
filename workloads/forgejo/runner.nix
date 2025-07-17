cb75c76198f15c33b098a8b2a95a284c1ea13b35
Rutger Putter <rutger@prutser.net>
2025-07-17 11:22:49 +0200

    chore: fix runner secret

--- workloads/forgejo/runner.nix
+++ workloads/forgejo/runner.nix
@@ -7,17 +7,14 @@
     package = pkgs.forgejo-actions-runner;
     instances.default = {
       enable = true;
-      name = "lxc-forge-runner";
+      name = "vm-forge-runner";
       url = "https://forge.intern.prutser.net";
       # Obtaining the path to the runner token file may differ
       # tokenFile should be in format TOKEN=<secret>, since it's EnvironmentFile for systemd
       tokenFile = config.age.secrets.forgejo-runner-token.path;
       labels = [
-        "ubuntu-latest:docker://node:16-bullseye"
-        "ubuntu-22.04:docker://node:16-bullseye"
         # provide native execution on the host:
         "native:host"
-        "docker"
       ];
       settings = {
         container = {
@@ -54,12 +51,13 @@
         isSystemUser = false;
         isNormalUser = true;
         shell = pkgs.zsh;
-        extraGroups = [ "nixbld" ];
+        extraGroups = [ ];
       };
     };
   };
 
   systemd.services."gitea-runner-default".serviceConfig.ReadWritePaths = [
     "/home/gitea-runner"
+    "/tmp"
   ];
 }
