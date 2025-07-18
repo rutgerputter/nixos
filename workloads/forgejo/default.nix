cc06da969f50aeab44fb7709a1618a1cce3309d0
Rutger Putter <rutger@prutser.net>
2025-07-18 10:25:58 +0200

    feat(forge): enable SSH server

--- workloads/forgejo/default.nix
+++ workloads/forgejo/default.nix
@@ -28,7 +28,7 @@ in
         ROOT_URL = "https://${srv.DOMAIN}/";
         HTTP_PORT = 3000;
         START_SSH_SERVER = true;
-        SSH_PORT = 222;
+        SSH_PORT = 3222;
         SSH_DOMAIN = "vm-forge.services.prutser.net";
       };
       # You can temporarily allow registration to create an admin user.
