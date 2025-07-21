c6bea0e7c4a5cedee35c457e8586fef814e0ce22
Rutger Putter <rutger@prutser.net>
2025-07-21 14:42:58 +0200

    chore(tubesync): fix networking and dns

--- workloads/tubesync/default.nix
+++ workloads/tubesync/default.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 {
   imports = [
+    ../common/podman
     ./mounts.nix
   ];
 
@@ -15,13 +16,16 @@
           "/data/tubesync-config:/config"
           "/data/downloads:/downloads"
          ];
+      networks = [
+        "podman"
+      ];
       environmentFiles = [
         config.age.secrets.tubesync_env.path
       ];
       environment = {
-        TUBESYNC_RENAME_ALL_SOURCES = true;
-        TUBESYNC_POT_IPADDR = 10.0.10.108;
-        TUBESYNC_POT_PORT = 4416;
+        TUBESYNC_RENAME_ALL_SOURCES = "true";
+        TUBESYNC_POT_IPADDR = "tubesync-bgutil-provider";
+        TUBESYNC_POT_PORT = "4416";
         PUID = "99";
         PGID = "100";
         TZ = "Europe/Amsterdam";
@@ -32,19 +36,25 @@
       ];
     };
     tubesync-bgutil-provider = {
-      image = "ghcr.io/meeb/tubesync:latest";
+      image = "brainicism/bgutil-ytdlp-pot-provider:latest";
       autoStart = true;
+      networks = [
+        "podman"
+      ];
       ports = [ "4416:4416" ];
       environment = {
-        TOKEN_TTL = 6;
+        TOKEN_TTL = "6";
       };
     };
     tubesync-db = {
       image = "postgres:17";
       autoStart = true;
-      ports = [ "4416:4416" ];
+      ports = [ ];
       volumes = [
         "/data/tubesync-db:/var/lib/postgresql/data"
+      ];
+      networks = [
+        "podman"
       ];
       environmentFiles = [
         config.age.secrets.tubesync_env.path
