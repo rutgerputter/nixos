554cb9d953d0c7907eb3ac9024ef92104dbe6c43
Rutger Putter <rutger@prutser.net>
2025-07-25 08:56:09 +0200

    feat(lidarr): sqlite with NFS is slow, so only backup to NFS

--- workloads/lidarr/mounts.nix
+++ workloads/lidarr/mounts.nix
@@ -1,6 +1,6 @@
 { ... }:
 {
-  fileSystems."/data/lidarr-config" = {
+  fileSystems."/data/lidarr-config/Backups" = {
     device = "10.0.99.10:/mnt/ssd_pool/dockervols/arr/lidarr";
     fsType = "nfs";
     options = [ "nfsvers=4.2" "nolock" "soft" "rw" "nconnect=6" ];
