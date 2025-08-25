9a2a23ec6dcfe4ccfe7d9b0a7b1a068b8005c838
Rutger Putter <rutger@prutser.net>
2025-08-25 09:54:27 +0200

    feat(subgen): add subgen to bazarr for whisper-based subs

--- workloads/bazarr/default.nix
+++ workloads/bazarr/default.nix
@@ -2,6 +2,7 @@
 {
   imports = [
     ./mounts.nix
+    ./subgen.nix
   ];
 
   services.bazarr = {
---
+++ workloads/bazarr/subgen.nix
@@ -0,0 +1,44 @@
+{ ... }:
+{
+  imports = [
+    ../common/podman
+  ];
+
+  virtualisation.oci-containers.containers = {
+    sungen = {
+      image = "mccloud/subgen";
+      autoStart = true;
+      ports = [ "9000:9000" ];
+      volumes = [
+          "/data/subgen/models:/subgen/models"
+         ];
+      environment = {
+        WHISPER_MODEL = "medium";
+        WHISPER_THREADS = "4";
+        PROCADDEDMEDIA= "True";
+        PROCMEDIAONPLAY = "False";
+        NAMESUBLANG = "aa";
+        SKIPIFINTERNALSUBLANG = "eng";
+        JELLYFINTOKEN = "token here";
+        JELLYFINSERVER = "https://jellyfin.prutser.net";
+        WEBHOOKPORT = "9000";
+        CONCURRENT_TRANSCRIPTIONS = "2";
+        WORD_LEVEL_HIGHLIGHT = "False";
+        DEBUG = "True";
+        USE_PATH_MAPPING = "False";
+        TRANSCRIBE_DEVICE = "cpu";
+        CLEAR_VRAM_ON_COMPLETE = "True";
+        MODEL_PATH = "./models";
+        UPDATE = "False";
+        APPEND = "False";
+        USE_MODEL_PROMPT = "False";
+        CUSTOM_MODEL_PROMPT = "";
+        LRC_FOR_AUDIO_FILES = "True";
+        CUSTOM_REGROUP = "cm_sl=84_sl=42++++++1";
+        PUID = "99";
+        PGID = "100";
+        TZ = "Europe/Amsterdam";
+      };
+    };
+  };
+}
