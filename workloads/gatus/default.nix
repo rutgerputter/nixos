7f97cfa81cbcd7ce899c9fc88ddbefbfb5366ce9
Rutger Putter <rutger@prutser.net>
2025-08-25 15:40:09 +0200

    feat(gatus): move config to own yaml file for easy copy of docs

--- workloads/gatus/default.nix
+++ workloads/gatus/default.nix
@@ -9,54 +9,8 @@
     openFirewall = true;
     settings = {
       web.port = 8080;
-      endpoints = [
-        {
-          name = "prutser.net";
-          group = "websites";
-          alerts = [
-            {
-              type = "gotify";
-            }
-          ];
-          url = "https://www.prutser.net";
-          interval = "1m";
-          conditions = [
-            "[STATUS] == 200"
-            "[RESPONSE_TIME] < 300"
-          ];
-        }
-        {
-          name = "blog.prutser.net";
-          group = "websites";
-          url = "https://blog.prutser.net";
-          interval = "1m";
-          conditions = [
-            "[STATUS] == 200"
-            "[RESPONSE_TIME] < 300"
-          ];
-        }
-        {
-          name = "jellyfin.prutser.net";
-          group = "media stack";
-          url = "https://jellyfin.prutser.net";
-          interval = "1m";
-          conditions = [
-            "[STATUS] == 200"
-            "[RESPONSE_TIME] < 300"
-          ];
-        }
-      ];
-      alerting.gotify = {
-        server-url = "https://gotify.intern.prutser.net";
-        token = "Af6L_5tY7GRc9Xj";
-        default-alert = {
-          description = "health check failed";
-          send-on-resolved = true;
-          failure-threshold = 2;
-          success-threshold = 2;
-        };
-      };
     };
+    configFile = "./config.yaml";
   };
 
   environment.systemPackages = with pkgs; [
