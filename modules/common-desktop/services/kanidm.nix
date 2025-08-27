bdb1899f91be4017be7f4f1ad93c0a42afa422a2
Rutger Putter <rutger@prutser.net>
2025-08-27 15:06:35 +0200

    feat(flake): add client logins

--- modules/common-desktop/services/kanidm.nix
+++ modules/common-desktop/services/kanidm.nix
@@ -5,6 +5,20 @@
     enablePam = true;
     package = pkgs.kanidm_1_6;
     clientSettings.uri = "https://auth.realiz-it.nl";
-    unixSettings = builtins.readFile ./kanidm-unixd.toml;
+    unixSettings = {
+      version = "2";
+      default_shell = "/bin/zsh";
+      home_prefix = "/home/";
+      home_attr = "uuid";
+      kanidm = {
+        pam_allowed_login_groups = ["idm_all_persons"];
+        map_group = [
+          {
+            local = "wheel";
+            "with" = "posix_wheel";
+          }
+        ];
+      };
+    };
   };
 }
