07115ece04552a80464042a5ebf909eb3e0fefcd
Rutger Putter <rutger@prutser.net>
2025-08-27 14:59:52 +0200

    feat(flake): add client logins

--- modules/common-desktop/services/kanidm.nix
+++ modules/common-desktop/services/kanidm.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.kanidm = {
     enableClient = true;
+    enablePam = true;
+    package = pkgs.kanidm_1_6;
     clientSettings.uri = "https://auth.realiz-it.nl";
+    unixSettings = builtins.readFile ./kanidm-unixd.toml;
   };
 }
