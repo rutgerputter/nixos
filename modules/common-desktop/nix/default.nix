af89a6d2bdf01f692dc5f52aa389ba12eefe6163
Rutger Putter <rutger@prutser.net>
2025-08-29 20:37:17 +0200

    fix(pc-rputter): build

--- modules/common-desktop/nix/default.nix
+++ modules/common-desktop/nix/default.nix
@@ -4,10 +4,10 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       substituters = [
+        "https://cache.nixos.org?priority=10"
         "https://colmena.cachix.org"
         "https://nix-community.cachix.org"
       ];
-      trusted-substituters = [ "https://colmena.cachix.org"];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
