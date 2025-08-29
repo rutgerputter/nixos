980ef48355af62e14672322d99ee228b1519591e
Rutger Putter <rutger@prutser.net>
2025-08-29 11:00:50 +0200

    feat(users): rely completely on kanidm users

--- modules/common-desktop/home-manager/default.nix
+++ modules/common-desktop/home-manager/default.nix
@@ -8,7 +8,7 @@
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
       # Import your home-manager configuration
-      rputter = import ./rputter.nix;
+      "rutger.putter" = import ./rutger.putter.nix;
     };
   };
 }
