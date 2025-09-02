c9bd6e3d84b47690e310d8c4c961773c48ef2bbb
Rutger Putter <rputter@realiz-it.nl>
2025-09-02 15:26:40 +0200

    feat(programs): add zen-browser

--- modules/common-desktop/programs/zen-browser.nix
+++ modules/common-desktop/programs/zen-browser.nix
@@ -3,12 +3,4 @@
   environment.systemPackages = with pkgs; [
     inputs.zen-browser.packages."${system}".default # beta
   ];
-
-  inputs.zen-browser.packages."${system}".default.override {
-    policies = {
-      DisableAppUpdate = true;
-      DisableTelemetry = true;
-      # more and more
-    };
-  }
 }
