cad5e88c47ed25292bb5997d47de63441c9e20e3
Rutger Putter <rputter@realiz-it.nl>
2025-09-04 14:57:49 +0200

    feat(vr): add additional repo

--- modules/common-desktop/services/wivrn.nix
+++ modules/common-desktop/services/wivrn.nix
@@ -2,17 +2,9 @@
 {
   services.wivrn = {
     enable = true;
-    package = pkgs.unstable.wivrn.override { cudaSupport = true; };
+    package = pkgs.wivrn.override { cudaSupport = true; };
     openFirewall = true;
     defaultRuntime = true;
-    autoStart = true;
-  };
-
-  security.wrappers."wivrn-server" = {
-    setuid = false;
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_nice+eip";
-    source = "${pkgs.unstable.wivrn}/bin/wivrn-server";
+    autoStart = false;
   };
 }
