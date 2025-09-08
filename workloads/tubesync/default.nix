83f169f02974f17781abcb79be40d8ce0f8dbff9
Rutger Putter <rputter@realiz-it.nl>
2025-09-08 14:12:58 +0200

    fix(tubesync): set bgutil version

--- workloads/tubesync/default.nix
+++ workloads/tubesync/default.nix
@@ -9,7 +9,7 @@
 
   virtualisation.oci-containers.containers = {
     tubesync = {
-      image = "ghcr.io/meeb/tubesync:0.15.9";
+      image = "ghcr.io/meeb/tubesync:v0.15.9";
       autoStart = true;
       ports = [ "4848:4848" ];
       volumes = [
