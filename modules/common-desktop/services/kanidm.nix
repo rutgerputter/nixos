9ff5803a8919eec163886d575c98939b21ddfecd
Rutger Putter <rputter@realiz-it.nl>
2025-09-19 10:41:48 +0200

    feat(kanidm): add displaymanager depend

--- modules/common-desktop/services/kanidm.nix
+++ modules/common-desktop/services/kanidm.nix
@@ -20,4 +20,10 @@
       };
     };
   };
+
+  /* ensure display-manager is started after kanidm to allow direct logins */
+  systemd.services.display-manager = {
+    wants = [ "kanidm-unixd.service" ];
+    after = [ "kanidm-unixd.service" ];
+  };  
 }
