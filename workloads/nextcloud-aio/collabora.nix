7096c9f8224099b646a65ea22dea45983f571562
Rutger Putter <rputter@realiz-it.nl>
2025-10-13 08:34:36 +0200

    feat(nextcloud): change collabora

--- workloads/nextcloud-aio/collabora.nix
+++ workloads/nextcloud-aio/collabora.nix
@@ -1,17 +1,27 @@
 { ... }:
 {
-  virtualisation.oci-containers.containers.collabora = {
-    image = "docker.io/collabora/code:latest";
-    ports = [ "9980:9980/tcp" ];
-    autoStart = true;
-    environment = {
+  services.collabora-online = {
+    enable = true;
+    port = 8000; # default
+    settings = {
+      # Rely on reverse proxy for SSL
+      ssl = {
+        enable = false;
+        termination = true;
+      };
+
+      net = {
+        listen = "any";
+      };
+
+      # Restrict loading documents from WOPI Host nextcloud.example.com
+      storage.wopi = {
+        "@allow" = true;
+        host = ["cloud.realiz-it.nl"];
+      };
+
+      # Set FQDN of server
       server_name = "wopi.realiz-it.nl";
-      aliasgroup1 = "https://cloud.realiz-it.nl:443";
-      dictionaries = "nl_NL,en_US,de_DE,fr_FR";
-      extra_params = "--o:ssl.enable=false --o:ssl.termination=true --o:security.seccomp=true";
-    };
-    labels = {
-      "io.containers.autoupdate" = "registry";
     };
   };
 
@@ -19,58 +29,26 @@
     9980
     8000
   ];
-
-  services.nginx.virtualHosts."wopi.realiz-it.nl" = {
-    listen = [
-      {
-        addr = "0.0.0.0";
-        port = 8000;
-        ssl = false;
-      }
-    ];
-    extraConfig = ''
-      # static files
-
-      location ^~ /browser {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Host $host;
-      }
-
-      # WOPI discovery URL
-      location ^~ /hosting/discovery {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Host $host;
-      }
-
-      # Capabilities
-      location ^~ /hosting/capabilities {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Host $host;
-      }
-
-      # main websocket
-      location ~ ^/cool/(.*)/ws$ {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
-        proxy_set_header Host $host;
-        proxy_read_timeout 36000s;
-      }
-
-      # download, presentation and image upload
-      location ~ ^/(c|l)ool {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Host $host;
-      }
-
-      # Admin Console websocket
-      location ^~ /cool/adminws {
-        proxy_pass http://127.0.0.1:9980;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection "Upgrade";
-        proxy_set_header Host $host;
-        proxy_read_timeout 36000s;
-      }
-    '';
-  };
+#   services.nginx = {
+#     enable = true;
+#     # I recommend these, but it's up to you
+#     recommendedProxySettings = true;
+#     recommendedTlsSettings = true;
+#
+#     virtualHosts."wopi.realiz-it.nl" =  {
+#       listen = [
+#         {
+#           addr = "0.0.0.0";
+#           port = 8000;
+#           ssl = false;
+#         }
+#       ];
+#       enableACME = false;
+#       forceSSL = false;
+#       locations."/" = {
+#         proxyPass = "http://[::1]:${toString config.services.collabora-online.port}";
+#         proxyWebsockets = true; # collabora uses websockets
+#       };
+#     };
+#   };
 }
