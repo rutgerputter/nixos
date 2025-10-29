fc245c635e737aea62c4bb57f5c46bd09864d57a
Rutger Putter <rputter@realiz-it.nl>
2025-10-29 15:32:44 +0100

    fext(x1): add disk encryption

--- hosts/x1-rputter/modules/hardware/default.nix
+++ hosts/x1-rputter/modules/hardware/default.nix
@@ -16,6 +16,12 @@
     logitech.wireless.enable = true;
     # NVIDIA
     nvidia.open = true;
+    nvidia.prime = {
+      # Make sure to use the correct Bus ID values for your system!
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+      # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+  };
   };
 
   hardware.enableAllFirmware = true;
