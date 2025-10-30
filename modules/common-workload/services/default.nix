1093a7aac12620704650f0bab42076e4b2b830c4
Rutger Putter <rutger@prutser.net>
2025-10-30 21:51:08 +0100

    chore(kanidm): fix ssh logins

--- modules/common-workload/services/default.nix
+++ modules/common-workload/services/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }:
+{ config, ... }:
+
+let
+  aKC = "kanidm_ssh_authorizedkeys";
+in
+
 {
   imports = [
     ./kanidm.nix
@@ -7,12 +12,18 @@
   # The background OpenSSH daemon for remote SSH access to this host.
   services.openssh = {
     enable = true;
-    authorizedKeysCommand = "${pkgs.kanidm_1_7}/bin/kanidm_ssh_authorizedkeys %u";
+    authorizedKeysCommand = "/run/wrappers/bin/${aKC} %u";
     authorizedKeysCommandUser = "nobody";
     settings.PubkeyAuthentication = true;
     # require public key authentication for better security
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
     settings.UsePAM = true;
+  };
+
+  security.wrappers."${aKC}" = {
+    source = "${config.services.kanidm.package}/bin/${aKC}";
+    owner = "root";
+    group = "root";
   };
 }
